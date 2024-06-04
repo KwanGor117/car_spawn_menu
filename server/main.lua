59c5f88492fb0c201f563d79a346c902de1f8549
Cola <79643020+Who-is-Cola@users.noreply.github.com>
2024-06-04 14:34:51 +0800

    Add files via upload

--- server/main.lua
+++ server/main.lua
@@ -12,3 +12,21 @@ RegisterCommand('opencarmenu', function(source, args, rawCommand)
         xPlayer.showNotification('You do not have permission to use this command.')
     end
 end, false)
+
+--[[
+local function openCarMenu(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer.getGroup() == 'admin' or xPlayer.getGroup() == 'superadmin' then
+        TriggerClientEvent('car_spawn_menu:openMenu', source)
+    else
+        xPlayer.showNotification('You do not have permission to use this command.')
+    end
+end
+
+RegisterNetEvent('car_spawn_menu:keyPress')
+AddEventHandler('car_spawn_menu:keyPress', function()
+    local source = source
+    openCarMenu(source)
+end)
+
+]]
